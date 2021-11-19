92d848a49ee77da84558b242f6e0244169bb63dc
Pjery3 <mixxer1265755@gmail.com>
2021-11-19 16:51:09 +0300

    added about-tour section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-
+    $('body').on('click', '.video-preview-wrapper', function(e){
+        let video = $('#about-tour-video');
+        let videoSrc = video.attr('src');
+        $(this).css('display', 'none')
+        video.attr('src', videoSrc + '?autoplay=1')
+    })
 });
